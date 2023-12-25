ee47be3bf48505587c6a51ca351c0731e7f4ebf3
shahbaz yasin <shahbazyasin26@gmail.com>
2023-12-24 21:22:39 -0800

    login-signup

---
+++ app.js
@@ -0,0 +1,18 @@
+function signIn() {
+    var firstName = document.getElementById("fName").value;
+    var pass = document.getElementById("passward").value;
+
+    console.log('firshName')
+    console.log('pass')
+}
+function signUp() {
+    var firshName = getElementById("fName").value;
+    var lastName = getElementById("lName").value;
+    var email = getElementById("emailId").value;
+    var passward = getElementById("passward").value;
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+    console.log(passward)
+}
