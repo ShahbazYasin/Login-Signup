cbb78189a9b8db0be26437fd41996d0a1b22c9d1
shahbaz yasin <shahbazyasin26@gmail.com>
2023-12-26 09:13:55 -0800

    login page updated 12-12-2023

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ function signIn() {
     var firstName = document.getElementById("fName").value;
     var pass = document.getElementById("passward").value;
 
-    console.log('firshName')
-    console.log('pass')
+    console.log(firstName);
+    console.log(pass);
 }
-function signUp() {
-    var firshName = getElementById("fName").value;
-    var lastName = getElementById("lName").value;
-    var email = getElementById("emailId").value;
-    var passward = getElementById("passward").value;
 
-    console.log(firstName)
-    console.log(lastName)
-    console.log(email)
-    console.log(passward)
+let UserData = function(userName, lastName, email, passward){
+    this.userName = userName
+    this.lastName = lastName
+    this.email = email
+    this.passward = passward
+}
+
+function signUp() {
+    var userName = document.getElementById("fName").value;
+    var lastName = document.getElementById("lName").value;
+    var email = document.getElementById("emailId").value;
+    var passward = document.getElementById("passward").value;
+
+    var data = new UserData(userName, lastName, email, passward)
+    console.log(data);
+
+
+    // console.log(userName);
+    // console.log(lastName);
+    // console.log(email);
+    // console.log(passward);
 }
